9fc5d8d76354121d65a3c3d4ad4eae1c781b6161
Filippo Ambrogiani <81953792+Filo17@users.noreply.github.com>
2024-03-20 09:51:43 +0100

    Add files via upload

---
+++ Ambrogiani.Filippo.4i.Gelati2/Ambrogiani.Filippo.4i.RubricaUnoAMolti/Gelato.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambrogiani.Filippo._4i.Rubrica
+{
+    public class Gelato
+    {
+        public int idGelato { get; set; } //è un ID o anche detto chiave primaria
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public double Prezzo { get; set; }
+        public Gelato()
+        {
+            idGelato = 1;
+        }
+
+        public Gelato(string riga)
+        {
+            string[] Campi = riga.Split(";"); //divido una riga csv
+            if (Campi.Count() != 4)
+                throw new Exception("Le righe del file Gelati.csv devono essere di 4 colonne");
+
+            int id = 0;
+            int.TryParse(Campi[0], out id); //converto la stringa in un numero
+            idGelato = id;
+
+            this.Nome = Campi[1];
+            this.Descrizione = Campi[2];
+            this.Prezzo = Convert.ToDouble(Campi[3]);
+
+        }
+    }
+    public class Gelati : List<Gelato> //rendo la classe una vera e propria lista
+    {
+        public Gelati() { }
+        public Gelati(string nomeFile)
+        {
+            StreamReader fin = new StreamReader(nomeFile);
+            fin.ReadLine();
+
+            while (!fin.EndOfStream)
+            {
+                this.Add(new Gelato(fin.ReadLine()));
+            }
+            fin.Close();
+        }
+    }
+}
---
+++ Ambrogiani.Filippo.4i.Gelati2/Ambrogiani.Filippo.4i.RubricaUnoAMolti/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Ambrogiani.Filippo._4i.Rubrica;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ambrogiani.Filippo._4i.RubricaUnoAMolti
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        Ingredienti ingredienti;
+        Gelati gelati;
+        List<Ingrediente> ListaVuota = new List<Ingrediente>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void dgDati_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            Gelato p = e.AddedItems[0] as Gelato; //salva le informazioni della tabella selezionata
+            if (p != null)
+            {
+                statusBar.Text = $"Nome: {p.Nome}";
+                testoIngredienti.Text = $"ID: {p.idGelato}";
+                        List<Ingrediente> ingredientiFiltrati = new List<Ingrediente>();
+                        foreach (var item in ingredienti)
+                        {
+                            if (item.idGelato == p.idGelato)
+                            {
+                                ingredientiFiltrati.Add(item);
+                            }
+                        }
+                dgIngredienti.ItemsSource = ingredientiFiltrati; //se selezionato un gelato mostro tutti gli ingredienti con lo stesso ID
+                    
+            }
+            else { 
+                statusBar.Text = "Nome: ...";
+                testoIngredienti.Text = "ID: ...";
+                dgIngredienti.ItemsSource = ListaVuota; //svuoto la lista se niente è selezionato
+            }
+        }
+            private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                gelati = new Gelati("Gelati.csv");
+                dgGelati.ItemsSource = gelati;
+
+                ingredienti = new Ingredienti("Ingredienti.csv");
+
+                statusBar.Text = $"Ho letto dal file {gelati.Count} gelati";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        private void dgGelati_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            Gelato p = e.Row.Item as Gelato;
+            if(p != null)
+            {
+                if (p.idGelato == 1)
+                {
+                   //e.Row.Background = Brushes.Red;
+                   //e.Row.Foreground = Brushes.Green;
+                }
+            }
+        }
+    }
+}
